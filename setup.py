32380d320b7788fc4778a445e5c00a62282cf23a
Kiripolszky Károly <k.kiripolszky@mito.hu>
2018-02-05 12:41:23 +0100

    setup.py update

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ from setuptools import setup
 
 if __name__ == "__main__":
     setup(name="txtable",
-          version="2.1.0",
-          description="Plain text tables in Python",
+          version="2.1.1",
+          description="Plain text tables for Python 3",
           long_description=open(os.path.join(os.path.dirname(__file__), "README.rst")).read(),
           author="Károly Kiripolszky",
           author_email="karcsi@ekezet.com",
